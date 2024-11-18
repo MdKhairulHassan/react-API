539db137165f40cc87c71459d7a3c889f67150e1
MdKhairulHassan <darknessmoon76@gmail.com>
2024-11-18 12:22:39 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  
+  let [all, setAll] = useState([])
+  
+  useEffect(() => {
+    
+    async function all() {
+      let data = await axios.get(" https://restcountries.com/v3.1/all");
+      setAll(data.data)
+      // console.log(data.data);
+    }
+    all()
+
+  },[])
+  return (
+    <>
+      
+      <select>
+        {all.map(item =>
+          <option>
+            {item.region}
+          </option>
+        )}
+      </select>
+
+    </>
+  )
+}
+
+export default App
